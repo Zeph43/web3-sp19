62a1d225fb9948437a5cf457b4045ee555470e38
Zeph43 <47579288+Zeph43@users.noreply.github.com>
2019-04-02 04:57:29 -0700

    Midterm
    The entire midterm project with home, about, fashion, designers pages. Plus css.

---
+++ web3-wk6/main.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+    $('.carousel').carousel({
+        interval: 1000,
+        keyboard: false
+    })
+
+    $(".carousel-pauser").click(function () {
+        $('.carousel').carousel('pause')
+    });
+
+        $('.carousel').on('slid.bs.carousel', function (event) {
+            console.log(event)
+        });
+
+        $('.modal-popper').click(function () {
+            $('.modal').modal({
+                backdrop: 'static'
+            });
+        });
+    });
