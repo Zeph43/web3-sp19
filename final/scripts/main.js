db54233be11313e1c54a0f1f658a717a5fbbbec1
Zeph43 <47579288+Zeph43@users.noreply.github.com>
2019-06-04 12:57:37 -0700

    re-organzed repo folders
    renamed a folder, and place old final stuff in a new folder

--- final/scripts/main.js
+++ final/scripts/main.js
@@ -1,8 +1,3 @@
-$( document ).ready(function(){
-    $("#fade").fadeTo("slow", 0.5);
-    $("#noFade").fadeTo("slow", 0.5);
-  });
-
   $(".carousel-caption .btn-primary").click(function () {
     if (window.location.hash) {
           var wndHash = window.location.hash;
@@ -27,78 +22,38 @@ $( document ).ready(function(){
        $(".hamburger .center").toggleClass("hamburger-center");
      });
 
-     $("#nav-tabs li").click(function () {
-      if (window.location.hash) {
-            var wndHash = window.location.hash;
-            var tab = wndHash + "Tab";
-            // if hash exist on doc ready then remove class "active" from all tabs        
-            $("#tabs li").removeClass('active');
-            // show the tab content and make tab list item (li) active
-            $(wndHash).fadeIn();            
-            $(tab).parent().addClass("active");
-            console.log(wndHash, $(wndHash), tab, $(tab));
-        } else {
-            // if no hash make the BeforeTab active
-            $("#streetwear").fadeIn();            
-            $("#streetwear-tab").parent().addClass("active");
-        }};
 
-// JQUERY SHOPPING CART
+// A rather useful page: https://developer.mozilla.org/en-US/docs/Learn/HTML/Forms/Form_validation
 
-var goToCartIcon = function($addTocartBtn){
-  var $cartIcon = $(".my-cart-icon");
-  var $image = $('<img width="30px" height="30px" src="' + $addTocartBtn.data("image") + '"/>').css({"position": "fixed", "z-index": "999"});
-  $addTocartBtn.prepend($image);
-  var position = $cartIcon.position();
-  $image.animate({
-    top: position.top,
-    left: position.left
-  }, 500 , "linear", function() {
-    $image.remove();
+function changeButtonAndSendIfValid() {
+  // check that all the fields are valid
+  // if they are, turn off the button and send the form
+
+  var canWeSend = 1;
+
+  var checkFields = document.querySelectorAll("input");
+  var checkText = document.querySelector("textarea");
+
+  checkFields.forEach(function(currentField) {
+    if (currentField.validity.valid !== true) {
+      canWeSend = 0;
+    }
   });
+
+  if (checkText.validity.valid == false) {
+    canWeSend = 0;
+  }
+
+  console.log("can we send? " + canWeSend);
+
+  if (canWeSend == 1) {
+    this.classList.add("js-disable-button");
+    //this.disabled = true;
+    this.value = "one moment...";
+    // change this to 'false' if you want to check the styling of your button
+    return true;
+  }
 }
 
-$('.my-cart-btn').myCart({
-  currencySymbol: '$',
-  classCartIcon: 'my-cart-icon',
-  classCartBadge: 'my-cart-badge',
-  classProductQuantity: 'my-product-quantity',
-  classProductRemove: 'my-product-remove',
-  classCheckoutCart: 'my-cart-checkout',
-  affixCartIcon: true,
-  showCheckoutModal: true,
-  numberOfDecimals: 2,
-  cartItems: [
-    {id: 1, name: 'product 1', summary: 'summary 1', price: 10, quantity: 1, image: 'images/img_1.png'},
-    {id: 2, name: 'product 2', summary: 'summary 2', price: 20, quantity: 2, image: 'images/img_2.png'},
-    {id: 3, name: 'product 3', summary: 'summary 3', price: 30, quantity: 1, image: 'images/img_3.png'}
-  ],
-  clickOnAddToCart: function($addTocart){
-    goToCartIcon($addTocart);
-  },
-  afterAddOnCart: function(products, totalPrice, totalQuantity) {
-    console.log("afterAddOnCart", products, totalPrice, totalQuantity);
-  },
-  clickOnCartIcon: function($cartIcon, products, totalPrice, totalQuantity) {
-    console.log("cart icon clicked", $cartIcon, products, totalPrice, totalQuantity);
-  },
-  checkoutCart: function(products, totalPrice, totalQuantity) {
-    var checkoutString = "Total Price: " + totalPrice + "\nTotal Quantity: " + totalQuantity;
-    checkoutString += "\n\n id \t name \t summary \t price \t quantity \t image path";
-    $.each(products, function(){
-      checkoutString += ("\n " + this.id + " \t " + this.name + " \t " + this.summary + " \t " + this.price + " \t " + this.quantity + " \t " + this.image);
-    });
-    alert(checkoutString)
-    console.log("checking out", products, totalPrice, totalQuantity);
-  },
-  getDiscountPrice: function(products, totalPrice, totalQuantity) {
-    console.log("calculating discount", products, totalPrice, totalQuantity);
-    return totalPrice * 0.5;
-  }
-});
-
-$("#addNewProduct").click(function(event) {
-  var currentElementNo = $(".row").children().length + 1;
-  $(".row").append('<div class="col-md-3 text-center"><img src="images/img_empty.png" width="150px" height="150px"><br>product ' + currentElementNo + ' - <strong>$' + currentElementNo + '</strong><br><button class="btn btn-danger my-cart-btn" data-id="' + currentElementNo + '" data-name="product ' + currentElementNo + '" data-summary="summary ' + currentElementNo + '" data-price="' + currentElementNo + '" data-quantity="1" data-image="images/img_empty.png">Add to Cart</button><a href="#" class="btn btn-info">Details</a></div>')
-});
-});
+var getButton = document.querySelector('form input[type="submit"]');
+getButton.onclick = changeButtonAndSendIfValid;
