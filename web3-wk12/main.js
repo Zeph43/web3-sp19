121a4c67aa73c04bd64134e783b35ddd685d8ffa
Zeph43 <47579288+Zeph43@users.noreply.github.com>
2019-06-05 11:54:03 -0700

    week 9
    changed attribute on click. But it's only going to the last if statement. Want it to go to orange and bigger first, then smaller and blue.

---
+++ web3-wk12/main.js
@@ -0,0 +1,71 @@
+var canvas = document.getElementById("Game");
+var context = canvas.getContext("2d");
+
+var ballX = canvas.width/2;
+var ballY = canvas.height/2;
+var ballColor = 'rgb(0, 155, 155)';
+
+var ballRadius = 50;
+
+var speedX = 5;
+var speedY = 3;
+var moveRight = true;
+var moveDown = true;
+
+var turqouiseColor = 'rgb(0, 155, 155)';
+var orangeColor = 'orange';
+var blueColor = 'blue';
+
+function animate(){
+    context.fillStyle = 'rgba(255, 255, 255, 0.6)';
+    context.fillRect(0, 0, canvas.width, canvas.height);
+
+    //draw the ball
+    context.beginPath();
+    context.fillStyle = ballColor;
+    context.strokeStyle = 'black';
+    context.arc(ballX, ballY, ballRadius, 0, 2*Math.PI);
+    context.fill();
+    context.stroke();
+
+    if(ballX + ballRadius >= canvas.width || (ballX - ballRadius <= 0) ){
+        moveRight = !moveRight;
+    }
+
+    if(ballY + ballRadius >= canvas.height || ballY - ballRadius <= 0){
+        moveDown = !moveDown;
+    }
+
+    if(moveRight){
+        ballX = ballX + speedX;
+    } else {
+        ballX = ballX - speedX;
+    }
+
+    if(moveDown){
+        ballY = ballY + speedY;
+    } else {
+        ballY = ballY - speedY;
+    }
+
+    window.requestAnimationFrame(animate);
+};
+
+animate();
+
+canvas.addEventListener('click', function(event){ 
+    // find the absolute value of the distance of the click from the ball
+    // 
+    var distX = Math.abs(ballX - event.clientX);
+    var distY = Math.abs(ballY - event.clientY)
+
+    if( distX < ballRadius && distY < ballRadius){
+        ballColor = orangeColor
+        ballRadius = 80;
+    }
+
+    if( ballColor = orangeColor || turqouiseColor  ){
+        ballColor = blueColor;
+        ballRadius = 10;
+    }
+});
